402b1179137dffb6410bc2d4e11d736611438ee5
Will Sheffler <willsheffler@gmail.com>
2017-03-04 04:17:45 -0800

    WIP travis-ci, try hana-based config try4

--- setup.py
+++ setup.py
@@ -133,6 +133,9 @@ class CMakeBuild(build_ext):
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable,
                       ]
+        if 'CMAKE_OPTIONS' in os.environ:
+            print('setup.py add CMAKE_OPTIONS:', os.environ['CMAKE_OPTIONS'])
+            cmake_args += os.environ['CMAKE_OPTIONS'].split()
         ncpu = multiprocessing.cpu_count()
         if which('ninja'):
             cmake_args.append('-GNinja')
