3e89750f32c5397c0f2e3ef92cbc931881522c8d
Will Sheffler <willsheffler@gmail.com>
2017-03-13 14:54:32 -0700

    fix travis not detecting build failure

--- tools/build_and_test.py
+++ tools/build_and_test.py
@@ -7,4 +7,9 @@ from build_utils import build_and_test
 
 if __name__ == '__main__':
     print('== build_and_test.py start ==')
-    sys.exit(build_and_test())
+    try:
+        sys.exit(build_and_test())
+    except Exception as e:
+        print("error running build_and_test")
+        with open('.ERROR', 'w') as out:
+            out.write(str(e))
