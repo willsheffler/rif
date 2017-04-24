0d8ce3b1e137504d38020789b10a7fef16d973f0
Will Sheffler <willsheffler@gmail.com>
2017-04-23 20:25:03 -0700

    travis, try to get traceback

--- tools/build_and_test.py
+++ tools/build_and_test.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import sys
 import os
+import traceback
 sys.path.append(os.path.dirname(__file__))  # not sure why sometimes necessary
 from build_utils import build_and_test, build_and_run_gtest_auto
 
@@ -15,19 +16,20 @@ if __name__ == '__main__':
             sys.exit(build_and_test())
         except Exception as e:
             t, v, tb = sys.exc_info()
+            with open('.ERROR', 'w') as out:
+                out.write(str(e))
+                out.write(traceback.print_tb(tb)
             print('==========================================================')
             print("error running build_and_test, traceback:")
             print('==========================================================')
             print(e)
-            tb.print_tb()
-            with open('.ERROR', 'w') as out:
-                out.write(str(e))
+            traceback.print_tb(tb)
     else:
         try:
             build_and_run_gtest_auto()
-            gtest_ran = True
+            gtest_ran=True
         except NotImplementedError:
-            gtest_ran = False
+            gtest_ran=False
         print("gtest_ran:", gtest_ran)
         if not gtest_ran:
             build_and_test()
