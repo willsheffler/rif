0bf3b827437453f0d82f2bd7251989eded3b56ca
Will Sheffler <willsheffler@gmail.com>
2018-01-01 09:47:18 -0800

    [skip ci] worm multiprocess working? tweaks2

--- src/rif/apps/examples/worms_example.py
+++ src/rif/apps/examples/worms_example.py
@@ -1,11 +1,12 @@
 from rif.worm import *
 from rif.data import poselib
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from time import perf_counter
 
 
 def main():
     try: Nseg = int(sys.argv[-2])
-    except: Nseg = 14
+    except: Nseg = 12
     try: Nworker = int(sys.argv[-1])
     except: Nworker = 0
 
@@ -19,15 +20,22 @@ def main():
     segments = ([Segment([helix], exit='C'), ]
                 + [Segment([helix], entry='N', exit='C')] * (Nseg - 2)
                 + [Segment([helix], entry='N')])
+    t = perf_counter()
     worms = grow(segments,
                  SegmentXform('C1', lever=20),
                  thresh=5, max_workers=Nworker,
                  executor=ProcessPoolExecutor)
+    t = perf_counter() - t
     s = worms.scores
 
-    print('npass', len(s))
-    print('ptile', np.percentile(s, range(0, 100, 20)))
-    print('best5', s[:5])
+    try: ptile = np.percentile(s, range(0, 100, 20))
+    except: ptile = []
+    print('ptile', ptile)
+    print('best10', s[:10])
+    print('nseg', Nseg,
+          'npass', len(s),
+          'best', s[0] if s else 999,
+          'tgrow', t)
 
 if __name__ == '__main__':
     main()
